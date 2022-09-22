8815e3a3877e562709e60cc178d63ec2a255f3a1
whitchakara <whitneywilkerson@MacBook-Pro-2.local>
2022-09-22 16:45:49 -0500

    finished views and urls page, added user template

--- skills_app/views.py
+++ skills_app/views.py
@@ -75,3 +75,40 @@ def create(request):
             )
             
         return redirect("/dashboard")
+
+def one_skill(request,skill_id):
+    context = {
+        'skill': Skill.objects.get(id = skill_id)
+    }
+    return render(request, "skill.html", context)
+
+def update(request, skill_id):
+    errors = Skill.objects.skill_validator(request.POST)
+
+    if len(errors) > 0  :
+        for key, value in errors.items():  
+            messages.error(request, value)
+        return redirect('/dashboard')
+    else :
+        skill_update = Skill.objects.get(id = skill_id)
+        skill_update.location = request.POST['location']
+        skill_update.skill = request.POST['skill']
+        skill_update.profession = request.POST['profession']
+        skill_update.time = request.POST['time']
+        skill_update.save()
+        #return render(request, "edit.html") 
+        return redirect("/dashboard")
+
+def delete(request, skill_id):
+    user = User.objects.get(id = request.session['user_id'])
+    skill = Skill.objects.filter(id = skill_id)
+    #if job.id == user.id:
+    skill.delete()#is_removed = True delete()
+    #job.save()
+    return redirect('/dashboard')
+
+def profile(request, user_id):
+    context = {
+        'user': User.objects.get(id = user_id)
+    }
+    return render(request, "user.html", context)
